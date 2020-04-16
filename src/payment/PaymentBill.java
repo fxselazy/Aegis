05506a57a9eee0e519fa5b735d5386262e1b8daf
Nirawat Fuse <nirawat.fuse@mail.kmutt.ac.th>
2020-04-17 01:37:48 +0700

    [UPDATE] Construc PaymentBill

--- src/payment/PaymentBill.java
+++ src/payment/PaymentBill.java
@@ -1,11 +1,17 @@
 package payment;
 
+import account.Position;
+import person.Person;
 import service.RegisterCourses;
 
 public class PaymentBill extends RegisterCourses {
 
     private double totalPay;
     private RegisterCourses registerCourses;
+
+    public PaymentBill(long Id, String password, Person person, Position position) {
+        super(Id, password, person, position);
+    }
 
     @Override
     public String toString() {
