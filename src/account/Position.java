58a4875d5dc8593f840741e380dfd2062897f75e
Nirawat Fuse <nirawat.fuse@mail.kmutt.ac.th>
2020-04-17 01:10:52 +0700

    [UPDATE] package in Position

--- src/account/Position.java
+++ src/account/Position.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.agis;
+package account;
 
-/**
- *
- * @author Lenovo
- */
- enum Position {
-    DEPARTMENT , STUDENT;
+public enum Position {
+    DEPARTMENT, STUDENT
 }
