d59afcd5ae2b7055586fa882c5807502dbabde25
Phu <pusit.20574@mail.kmutt.ac.th>
2020-04-16 21:25:37 +0700

    [UPDATE] Policy

--- src/policy/Policy.java
+++ src/policy/Policy.java
@@ -1,6 +1,10 @@
-package policy;
 
-public interface class StudentAccount{
-    public static final int MaxCourses = 7;
-    public static final int MinCourses = 3;
+package javaapplication3;
+
+
+public interface Policy {
+    
+    public static final int MAX_COURSES = 7;
+    public static final int MIN_COURSES = 3;
+    
 }
