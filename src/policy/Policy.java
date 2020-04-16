776cf54b1024a7d1eea771bcc80323d87e194f4d
Nirawat Fuse <nirawat.fuse@mail.kmutt.ac.th>
2020-04-17 01:12:11 +0700

    [UPDATE] package in Policy

--- src/policy/Policy.java
+++ src/policy/Policy.java
@@ -1,10 +1,8 @@
-
-package javaapplication3;
-
+package policy;
 
 public interface Policy {
-    
+
     public static final int MAX_COURSES = 7;
     public static final int MIN_COURSES = 3;
-    
+
 }
