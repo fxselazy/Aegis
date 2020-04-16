2c534f7cfe29d72d0bfb3be6c9d18f03271797e3
Traitawat <traitawat.25957@mail.kmutt.ac.th>
2020-04-16 21:19:41 +0700

    [UPDATE]AccPos

--- src/account/Position.java
+++ src/account/Position.java
@@ -1,5 +1,14 @@
-package account;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.agis;
 
-public enum Position {
-    DEPARTMENT, STUDENT
+/**
+ *
+ * @author Lenovo
+ */
+ enum Position {
+    DEPARTMENT , STUDENT;
 }
