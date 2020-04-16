8a11c52f0b3e99af36fec7770b17c2aad8384131
Phu <pusit.20574@mail.kmutt.ac.th>
2020-04-16 21:28:19 +0700

    [UPDATE] Courses

--- src/courses/Courses.java
+++ src/courses/Courses.java
@@ -1,31 +1,32 @@
-package courses;
+
+package javaapplication3;
 
 public class Courses {
-    private String subject;
-    private int credit;
-    private float cost;
 
-    public Courses(String subject, int credit, float cost) {
-        this.subject = subject;
-        this.credit = credit;
-        this.cost = cost;
+    private String subject;
+    private int credits;
+    private final int cost = 1200;
+
+    public Courses(String subject, int credits) {
+        this.subject = subject != null ? subject : null;
+        this.credits = credits > 0 ? credits : -1;
     }
 
     public String getSubject() {
         return subject;
     }
 
-    public int getCredit() {
-        return credit;
+    public int getCredits() {
+        return credits;
     }
 
-    public float getCost() {
+    public int getCost() {
         return cost;
     }
 
     @Override
     public String toString() {
-        return "Courses{" + "subject=" + subject + ", credit=" + credit + ", cost=" + cost + '}';
+        return "Courses{" + "subject=" + subject + ", credits=" + credits + ", cost=" + cost + '}';
     }
     
     
