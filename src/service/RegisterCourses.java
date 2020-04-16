61173b69ddcc6e824b5cfd3f5d2250762f6850d3
Nirawat Fuse <nirawat.fuse@mail.kmutt.ac.th>
2020-04-17 01:12:42 +0700

    [CHANGE] RegisterCoursesFile

---
+++ src/service/RegisterCourses.java
@@ -0,0 +1,25 @@
+package service;
+
+import account.Account;
+import account.Position;
+import account.StudentAccount;
+import courses.Courses;
+import person.Person;
+
+public class RegisterCourses extends StudentAccount {
+
+    private StudentAccount studentAccount;
+    private Courses[] courses;
+    private int count;
+    private double TotalPay;
+
+//    public RegisterCourses(StudentAccount studentAccount,Courses courses) {
+//
+//    }
+//    public RegisterCourses(Account account) {
+//        super(account);
+//    }
+//    public RegisterCourses(long Id, String password, Person person, Position position) {
+//        super(Id, password, person, position);
+//    }
+}
