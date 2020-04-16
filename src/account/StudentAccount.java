401234972c3eab0712753645591b3de8e3b6d381
Nirawat Fuse <nirawat.fuse@mail.kmutt.ac.th>
2020-04-17 01:11:09 +0700

    [UPDATE] package in StudentAccount

--- src/account/StudentAccount.java
+++ src/account/StudentAccount.java
@@ -1,15 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.agis;
+package account;
 
-/**
- *
- * @author Lenovo
- */
-public class StudentAccount extends Account{
+import activity.Activity;
+import payment.PaymentStatus;
+import payment.PaymentBill;
+import person.Person;
+import service.RegisterCourses;
+
+public class StudentAccount extends Account {
+
     private RegisterCourses[] registerCourses;
     private int coursesCount;
     private Activity[] activity;
@@ -17,19 +15,28 @@ public class StudentAccount extends Account{
     private PaymentStatus paymentStatus;
     private int myActivityHour;
 
+    public StudentAccount(long Id, String password, Person person, Position position) {
+        super(Id, password, person, position);
+    }
+
+    public StudentAccount(Account account) {
+        super(account);
+    }
+
     public RegisterCourses[] getMyRegisterCourses() {
         return registerCourses;
     }
 
     public String getCountOfCourseForRegister() {
         int x = paymentStatus.MAX_COURSES - coursesCount;
-        
-        if(this.coursesCount < 3){
-            return  "You must register at least " + (x - 4) +" times";
-        }if(this.coursesCount == 7){
+
+        if (this.coursesCount < 3) {
+            return "You must register at least " + (x - 4) + " times";
+        }
+        if (this.coursesCount == 7) {
             return "Your courses to rigister are max.";
         }
-        return "You can register for " + x +  " more times.";
+        return "You can register for " + x + " more times.";
     }
 
     public PaymentBill[] getPaymentBill() {
@@ -43,9 +50,5 @@ public class StudentAccount extends Account{
     public int getMyActivityHour() {
         return myActivityHour;
     }
-    
-    
-    
-    
-    
+
 }
