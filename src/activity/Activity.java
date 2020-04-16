6413b9c5950652c90227b6b04d5631473fe173e5
Phu <pusit.20574@mail.kmutt.ac.th>
2020-04-16 21:28:38 +0700

    [UPDATE] Activity

--- src/activity/Activity.java
+++ src/activity/Activity.java
@@ -1,5 +1,25 @@
-package activity;
+
+package javaapplication3;
+
+import java.time.LocalDate;
+
 
 public class Activity {
+    private LocalDate hour;
+    private String nameOfActivity;
+
+    public Activity(LocalDate hour, String nameOfActivity) {
+        this.hour = hour;
+        this.nameOfActivity = nameOfActivity != null ? nameOfActivity : null;
+    }
+
+    public String getNameOfActivity() {
+        return nameOfActivity;
+    }
+
+    @Override
+    public String toString() {
+        return "Activity{" + "hour=" + hour + ", nameOfActivity=" + nameOfActivity + '}';
+    }
     
 }
