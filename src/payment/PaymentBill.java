20370bf1ebc731c779e5086fdf3c6e91928b9aaa
Phu <pusit.20574@mail.kmutt.ac.th>
2020-04-16 21:27:46 +0700

    [UPDATE] PPayBill

--- src/payment/PaymentBill.java
+++ src/payment/PaymentBill.java
@@ -1,5 +1,15 @@
-package payment;
 
-public class PaymentBill {
+package javaapplication3;
+
+
+public class PaymentBill extends RegisterCourses {
+    private double totalPay;
+    private RegisterCourses registerCourses;
+
+    @Override
+    public String toString() {
+        return "PaymentBill{" + "totalPay=" + totalPay + ", registerCourses=" + registerCourses + '}';
+    }
+    
     
 }
