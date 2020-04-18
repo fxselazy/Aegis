541e20a6b93c37ba0a7aa209114076746083a5f3
Traitawat <actlook55@gmail.com>
2020-04-18 21:04:03 +0700

    [UPDATE]Method in DepartService

--- src/service/DepartmentService.java
+++ src/service/DepartmentService.java
@@ -1,5 +1,22 @@
 package service;
 
-public class DepartmentService {
-    
+import account.Account;
+import account.DepartmentAccount;
+import account.StudentAccount;
+import activity.Activity;
+import courses.Courses;
+import payment.PaymentStatus;
+
+public interface DepartmentService {
+    public boolean addCourses(DepartmentAccount department,Courses course);
+    public boolean addMember(DepartmentAccount department,Account account);
+    public boolean addActivity(DepartmentAccount department,Activity activity);
+    public boolean addActivityHour(DepartmentAccount department,Activity activity,int hour);
+    public boolean removeActivity(DepartmentAccount department,Activity activity);
+    public boolean removeCourse(DepartmentAccount department,Courses course);
+    public boolean changePaymentStatus(DepartmentAccount department,StudentAccount account,PaymentStatus status);
+
+
+
+
 }
