722c6db41dfade06be9c406941d4bfb2dad78d27
Traitawat <actlook55@gmail.com>
2020-04-21 22:05:46 +0700

    [UPDATE]equels in Activity

--- src/activity/Activity.java
+++ src/activity/Activity.java
@@ -1,6 +1,7 @@
 package activity;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Activity {
 
@@ -22,6 +23,30 @@ public class Activity {
     public String getNameOfActivity() {
         return nameOfActivity;
     }
+
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Activity other = (Activity) obj;
+        if (!Objects.equals(this.nameOfActivity, other.nameOfActivity)) {
+            return false;
+        }
+        if (!Objects.equals(this.hour, other.hour)) {
+            return false;
+        }
+        return true;
+    }
+    
 
     @Override
     public String toString() {
