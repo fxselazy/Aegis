7550a06bb0ed59def55bcd93f43d00fbc3a5945a
Traitawat <actlook55@gmail.com>
2020-04-21 22:03:14 +0700

    [UPPDATE]Interface method in Departmentser

--- src/service/DepartmentService.java
+++ src/service/DepartmentService.java
@@ -9,9 +9,9 @@ import payment.PaymentStatus;
 
 public interface DepartmentService {
     public boolean addCourses(DepartmentAccount department,Courses course);
-    public boolean addMember(DepartmentAccount department,Account account);
+    public boolean addMember(DepartmentAccount department,StudentAccount account);
     public boolean addActivity(DepartmentAccount department,Activity activity);
-    public boolean addActivityHour(DepartmentAccount department,Activity activity,int hour);
+    public boolean addActivityHour(DepartmentAccount department,Activity activity,int hour,int studentNumber);
     public boolean removeActivity(DepartmentAccount department,Activity activity);
     public boolean removeCourse(DepartmentAccount department,Courses course);
     public boolean changePaymentStatus(DepartmentAccount department,StudentAccount account,PaymentStatus status);
