e2f468ddcc25048b7510bdb3f6832fe9389ac091
Phu <pusit.20574@mail.kmutt.ac.th>
2020-04-23 22:23:07 +0700

    [UPDATE] Method calculate in RegisterCourses

--- src/service/RegisterCourses.java
+++ src/service/RegisterCourses.java
@@ -1,17 +1,16 @@
 package service;
 
-import account.Account;
 import account.Position;
 import account.StudentAccount;
 import courses.Courses;
 import person.Person;
 
-public class RegisterCourses extends StudentAccount {
+public class RegisterCourses extends StudentAccount{
 
     private StudentAccount[] studentAccount;
     private Courses[] courses;
     private int count;
-    private double TotalPay;
+    private int TotalPay;
 
     public RegisterCourses(long Id, String password, Person person, Position position) {
         super(Id, password, person, position);
@@ -27,6 +26,15 @@ public class RegisterCourses extends StudentAccount {
 
     public int getCount() {
         return count;
+    }
+    
+    public int calculateForPay(){
+        int i;
+        for(i=0 ; i<courses.length; i++){
+            TotalPay += courses[i].getCalCost();
+        }
+        
+        return TotalPay;
     }
 
     public double getTotalPay() {
@@ -48,5 +56,18 @@ public class RegisterCourses extends StudentAccount {
         return sb.toString();
     }
     
-
+    public static void main(String[] args) {
+        Person p = new Person(076,"phu","pusit");
+        Courses c = new Courses("101","Compro",3);
+        
+        
+        RegisterCourses r = new RegisterCourses(001,"phu",p,Position.STUDENT);
+        
+        //iyukuiku
+        
+        System.out.println(c);
+        System.out.println(r.getTotalPay());
+                
+        
+    }
 }
