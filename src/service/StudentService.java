fddf17c10c3df4933bade1f7c3938a83cf250307
Traitawat <actlook55@gmail.com>
2020-04-25 22:43:14 +0700

    [UPDATE]fix interface method in StudentSer

--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -1,9 +1,11 @@
 package service;
 
 import account.StudentAccount;
+import courses.Courses;
 
 public interface StudentService {
     public double checkTotalCost(StudentAccount student);
-    public RegisterCourses getMyCourseTakenList(StudentAccount student);
+    public RegisterCourses[] getMyCourseTakenList(StudentAccount student);
     public boolean pay(StudentAccount student,double money);
+    public boolean registerCourse(StudentAccount student,RegisterCourses registerCourse);
 }
